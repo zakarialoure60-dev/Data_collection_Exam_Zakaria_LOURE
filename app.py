097ddeb86109cc67a4206c71402f5f7e47ee1ac7
zakarialoure60-dev <zakarialoure60@gmail.com>
2026-08-13 15:01:00 +0000

    Initialize Streamlit app for data collection

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+
+# Titre de l'application
+st.title("DATA COLLECTION EXAM")
+
+# Présentation
+st.write("Web Scraping, Data Visualization and SQL Application")
+
+st.write("Author: Zakaria LOURE")
+
+# Message de bienvenue
+st.success("Welcome to my Data Collection application!")
